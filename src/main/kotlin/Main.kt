aa7df9269e2536ec417db76b3ded33092c2e449b
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-03-25 08:24:11 +0300

    Added a kotlin assessment file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(){
+Calculate(4)
+ isPalindrome("madam")
+}
+
+
+// Question 1
+
+fun fullSentence(word:String){
+ var NewWord = word.()
+}
+
+//Question 2
+
+ val statistics = arrayOf(10,20,30){
+    println(statistics.sum())
+    println(statistics.size())
+    println(statistics.average())
+}
+
+
+// Question 3
+fun Calculate(r:Int){
+    var Volume = 4/3 * 3.14 * r*r*r
+    println(Volume)
+}
+
+// Question 4
+fun isPalindrome(word:String): Boolean{
+   if (word == word.reversed()){
+       return true
+   }
+    else{
+        return false
+   }
+}
